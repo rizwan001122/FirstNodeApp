7251550fed4deeca7ecf4030cf31c8e5d367dae4
rizwan001122 <mr7004589@gmail.com>
2024-06-13 12:47:42 +0500

    updated code

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const PORT = 8080;
 const app = express();
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world !" );
+    res.send( "Hello world" );
 } );
 const server = app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
